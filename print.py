d15bde88d3009d2b509180a2b0e30be6398ee882
HUH-HOON <66860841+HUH-HOON@users.noreply.github.com>
2021-04-21 22:49:13 +0900

    print

---
+++ print.py
@@ -0,0 +1,13 @@
+import heapq
+def solution(priorities, location):
+    heap = []
+    answer = 1
+    for item in priorities:
+        heapq.heappush(heap, (-item))
+    while heap:
+        for i in range(len(priorities)):
+            if priorities[i] == -heap[0]:
+                if i == location:
+                    return answer
+                heapq.heappop(heap)
+                answer += 1
